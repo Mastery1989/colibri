319586ef53e9948c6d1825f0a90963abbcc26644
jgunchenko <jgunchenko@gmail.com>
2013-08-21 19:59:06 +0400

    Project

---
+++ colibri/ColibriBand/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ColibriBand::Application.config.secret_key_base = '20d56e02f40335e8465fcd32e659a55acfdb177af94198da3608dce9d54a3def4409394fcc352b60e5f9feabb479b2f00d9d90b821c583acd8e9268ecba34f0e'
